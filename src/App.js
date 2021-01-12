688de6f60a91fc4aaae640eba1f4e53d714c3e4b
Johanna Horn <j.von.horn@hotmail.com>
2021-01-12 14:21:34 +0100

    Test Jumbotron

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import tree from "./tree.json";
 import "./components/TreeCard.css";
 import "./components/Search.css";
 import ProductList from "./components/ProductList";
+import Jumbo from './components/Jumbotron';
 
 function App() {
   return (
     <div className="App">
+      <Jumbo></Jumbo>
       <ProductList products={tree} />
     </div>
   );
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import  { Jumbotron, Container, Row, Col, Image, Button }  from 'react-bootstrap';
+
+
+export class Jumbo extends Component {
+    render() {
+        return (
+            <Jumbotron fluid>
+                <Container fluid>
+                    <Row> 
+                        <Col md={4}> </Col>
+                        <Col md={4}> <Image src="holder.js/171x180" rounded fluid /> </Col>
+                        <Col md={4}> 
+                            <h1>Intro</h1>
+                            <p>Intro Paragraph</p>
+                            <Button>Read me</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+
+        )
+    }
+}
+
+export default Jumbo
