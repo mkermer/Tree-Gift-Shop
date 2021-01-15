ec4ea243b0f913c9363cc70f1755183fceaa5903
Morgane Kermer <kermer.morgane@gmail.com>
2021-01-15 12:26:40 +0100

    add description to pic

--- src/components/ProductSection/TreeCard.js
+++ src/components/ProductSection/TreeCard.js
@@ -1,18 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
 import { Card, CardBody, CardTitle, CardText, CardImg, Col } from "reactstrap";
 import "./TreeCard.css";
 import CollapseDescription from './CollapseDiscription';
-
+import Collapse from 'react-bootstrap/Collapse';
 
 function TreeCard(props) {
+  const [open, setOpen] = useState(false);
   return (
       props.products.map((obj) => {
         return (
           <Col lg={4} xl={3} md={4} sm={12} xs={12} key={obj.id}>
             <Card>
-              <CardBody>
-                <CardImg src={obj.img} id="pic" alt="tree" />
+              <CardBody>        
+                <CardImg 
+                src={obj.img} 
+                id="pic" 
+                alt="tree" 
+                onClick={() => setOpen(!open)}
+                aria-controls="tree-description"
+                aria-expanded={open}
+                />
+                
                 <CardTitle> {obj.name}</CardTitle>
 
                 <CardText>
@@ -21,12 +30,17 @@ function TreeCard(props) {
                   </p>
                   <h3>{obj.price} $</h3>
                 </CardText>
-                <button id={obj.id} onClick={() => props.add(obj)}>
+                <Button className="ButtonAdd" id={obj.id} onClick={() => props.add(obj)}>
                   {" "}
                   Add{" "}
-                </button>
+                </Button>
                 <CollapseDescription/>
               </CardBody>
+              <Collapse in={open}>
+                <div id="tree-description">
+                  Tree picture and Tree description goes here!
+                </div>
+              </Collapse>
             </Card>
           </Col>
             
