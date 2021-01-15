0a052e4fdfbf7dd0a76898867306cd3c47286be3
Morgane Kermer <kermer.morgane@gmail.com>
2021-01-15 17:27:21 +0100

    Footer and Navbar fixed and changed

---
+++ src/components/FooTer/SignUp.js
@@ -0,0 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FormSignUp from '../NavigationBar/FormSignUp'
+
+function SignUp(){
+    return(
+        <>
+            
+                <h5>Sign up</h5><br/>
+            
+            <Row>
+                <Col>
+                <FormSignUp/>
+                <br/>
+                </Col>
+            </Row>
+            
+
+        </>
+        
+    );
+};
+
+export default SignUp;
