2b460747eeb03a343195673338894c0aeaa3020d
Adrian Fabjanski <adrianfab19@gmail.com>
2021-01-15 17:21:25 +0100

    login, cart fix, other stuff

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-app.use('/login', (req, res) => {
-    res.send({
-        token: 'access'
-    });
+app.use("/login", (req, res) => {
+  res.send({
+    token: "access",
+  });
 });
 
-app.listen(9000, () => console.log('API is running on http://localhost:9000/login'));
+app.listen(9000, () =>
+  console.log("API is running on http://localhost:9000/login")
+);
