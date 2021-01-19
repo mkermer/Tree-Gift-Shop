3b11002200c1c5b0da3f1a06eb2a911341ca553e
Adrian Fabjanski <adrianfab19@gmail.com>
2021-01-19 15:38:59 +0100

    connected trees to database

--- src/ProductContext.js
+++ src/ProductContext.js
@@ -1,67 +1,114 @@
-import React, { useState, createContext } from "react";
-import tree from "./tree";
+import React, { useState, createContext, useEffect } from "react";
+// import tree from "./tree";
+import Axios from 'axios'
 
 export const ProductContext = createContext();
 
 export const ProductProvider = (props) => {
-  const [products, setProducts] = useState(tree);
+  const [treeName, setTreeName] = useState('')
+    const [treeDesc, setTreeDesc] = useState('')
+    const [treeImg, setTreeImg] = useState('')
+    const [price, setPrice] = useState('')
+    const [country, setCountry] = useState('')
+    const [co2, setCo2] = useState('')
+    const [tree, setTrees] = useState()
+  // const [products, setProducts] = useState(tree);
+ 
 
+   const addTree = (e) => {
+    e.preventDefault();
+    Axios.post('http://localhost:9000/add', {
+      tree_name: treeName,
+      tree_description: treeDesc,
+      tree_img: treeImg,
+      price: price,
+      country: country,
+      co2: co2,
+    }
+    
+    )
+  }
+
+  const getTree = () => {
+    Axios.post('http://localhost:9000/getTree').then((response) => {
+      setTrees(response.data)
+    })
+  }
+
+//============Search==============//
   function handleChange(e) {
     var filteredProductList = tree.filter(
       (product) =>
-        product.name.toLowerCase().indexOf(e.target.value.toLowerCase()) >= 0
+        product.tree_name.toLowerCase().indexOf(e.target.value.toLowerCase()) >= 0
     );
-    setProducts(filteredProductList);
+    setTrees(filteredProductList);
   }
-
+//============Sorting==============//
   function sortByPriceDescending() {
-    let newProducts = [...products];
+    let newProducts = [...tree];
     let sorted = newProducts.sort((a, b) => b.price - a.price);
-    setProducts(sorted);
+    setTrees(sorted);
   }
 
   function sortByPriceAscending() {
-    let newProducts = [...products];
+    let newProducts = [...tree];
     let sorted = newProducts.sort((a, b) => a.price - b.price);
-    setProducts(sorted);
+    setTrees(sorted);
   }
 
   function sortByName() {
-    let newProducts = [...products];
-    let sorted = newProducts.sort((a, b) => a.name.localeCompare(b.name));
-    setProducts(sorted);
+    let newProducts = [...tree];
+    let sorted = newProducts.sort((a, b) => a.tree_name.localeCompare(b.tree_name));
+    setTrees(sorted);
   }
 
   function sortByCoAscending() {
-    let newProducts = [...products];
+    let newProducts = [...tree];
     let sorted = newProducts.sort((a, b) => a.co2 - b.co2);
-    setProducts(sorted);
+    setTrees(sorted);
   }
 
   function sortByCoDescending() {
-    let newProducts = [...products];
+    let newProducts = [...tree];
     let sorted = newProducts.sort((a, b) => b.co2 - a.co2);
-    setProducts(sorted);
+    setTrees(sorted);
   }
 
   function handleCountryChange(e) {
-    let sorted = [];
+    console.log(e.target.value)
     let newProducts = [...tree];
+    let sorted = [];
     newProducts.forEach((obj) => {
       if (obj.country.toLowerCase() === e.target.value) {
         return sorted.push(obj);
       } else if (e.target.value === "all") {
-        return (sorted = newProducts);
+        return (sorted = [...tree]);
       }
     });
-    setProducts(sorted);
+    setTrees(sorted);
   }
 
   return (
     <ProductContext.Provider
       value={{
-        products,
-        setProducts,
+        // products,
+        treeName,
+        setTreeName,
+        treeDesc,
+        setTreeDesc,
+        treeImg,
+        setTreeImg,
+        price,
+        setPrice,
+        getTree,
+        addTree,
+        country,
+        setCountry,
+        co2,
+        setCo2,
+        tree,
+        setTrees,
+        // setProducts,
         handleCountryChange,
         sortByCoDescending,
         sortByCoAscending,
--- src/components/ProductSection/TreeCard.js
+++ src/components/ProductSection/TreeCard.js
@@ -1,74 +1,81 @@
-import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { useState, useContext, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./TreeCard.css";
 import CollapseDescription from "./CollapseDiscription";
 import Collapse from "react-bootstrap/Collapse";
+import { ProductContext } from "../../ProductContext";
 
 function TreeCard(props) {
   const [open, setOpen] = useState(false);
-  
+  const {tree, getTree} = useContext(ProductContext)
+  const add = props.add
+  const collapse = (card) => {
+  }
+
+useEffect(() => {
+    getTree()
+  }, [])
+
+  const ListTrees = (props) => {
+    if(tree) {
+     return tree.map((obj) => {
+    return (
+      <>
+        <Col md={4} xs={12} key={obj.id}>
+          <Card className="TreeCard">
+            <Card.Img
+              variant="top"
+              src={obj.tree_img}
+              id="pic"
+              alt="tree"
+              onClick={() => setOpen(!open)}
+              aria-controls="tree-description"
+              aria-expanded={open}
+            />
+            <div className="shadow"></div>
+            <Card.Body>
+              <Card.Title> {obj.tree_name}</Card.Title>
+              <Card.Text>
+                <p>
+                  CO<sub>2</sub>: -{obj.co2}kg
+                </p>
+                <h3>{obj.price} $</h3>
+                <h3>{obj.country} $</h3>
+              </Card.Text>
+
+              <Button
+                className="ButtonAdd"
+                id={obj.id}
+                onClick={() => props.add(obj)}
+              >
+                Add
+              </Button>
+
+              <CollapseDescription />
+            </Card.Body>
+
+            <Collapse in={open}>
+              <div id="tree-description">
+                Tree picture and Tree description goes here!
+              </div>
+            </Collapse>
+          </Card>
+        </Col>
+      </>
+    );
+  });
+    } else {
+      return <></>
+    }
+  }
+
   return (
-      props.products.map((obj) => {
-        return (
-          <>
-
-         <Col md={4} xs={12} key={obj.id}>
-            <Card className="TreeCard" >   
-                <Card.Img 
-                variant="top"
-                src={obj.img} 
-                id="pic" 
-                alt="tree" 
-                onClick={() => setOpen(!open)}
-                aria-controls="tree-description"
-                aria-expanded={open}
-                />
-                <div className="shadow">
-                  
-                </div>
-                <Card.Body> 
-                
-                <Card.Title> {obj.name}</Card.Title>
-
-                <Card.Text>
-                  <p>
-                    CO<sub>2</sub>: -{obj.co2}kg
-                  </p>
-                  <h3>{obj.price} $</h3>
-                </Card.Text>
-
-                <Button 
-                  className="ButtonAdd" 
-                  id={obj.id} 
-                  onClick={() => props.add(obj)}>
-                  {" "}
-                  Add{" "}
-                </Button>
-
-                <CollapseDescription/>
-
-              </Card.Body>
-
-              <Collapse in={open}>
-                <div id="tree-description">
-                  Tree picture and Tree description goes here!
-                </div>
-              </Collapse>
-
-            </Card>
-
-          </Col>
-          </>
-            
-          
-        )
-      })
-  );
+    <ListTrees add={add}/>
+  )
 }
 
 export default TreeCard;
-
