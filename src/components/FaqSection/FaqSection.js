b4532e5a03401af6fffd03d09318fa7eb77dc3d0
Johanna Horn <j.von.horn@hotmail.com>
2021-01-19 13:25:09 +0100

    adding col + row to the accrodion header

--- src/components/FaqSection/FaqSection.js
+++ src/components/FaqSection/FaqSection.js
@@ -2,19 +2,38 @@ import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Col, Row } from 'react-bootstrap';
+
+import { ChevronBarDown } from "react-bootstrap-icons";
 
 import faq from './faq';
 
+import './FaqSection.css';
+
 function FaqSection(){
+
     return(
         <>
-        {faq.map((item, id) => (
+        {faq.map((item) => (
             <Accordion defaultActiveKey="0">
-                <Card>
+                <Card className="QuestionCard">
                     <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                            {item.question}
-                    </Accordion.Toggle>
+                        <Row> 
+                            <Col md={11}> 
+                                 <Accordion.Toggle as={Button} variant="link" eventKey="1">
+
+                                    {item.question} 
+
+                                </Accordion.Toggle>
+                            </Col>
+                            
+                            <Col md={1}> 
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    <ChevronBarDown size={25} />
+                                </Accordion.Toggle>
+                            </Col>
+                        </Row>
+
                     </Card.Header>
                     <Accordion.Collapse eventKey="1">
                         <Card.Body>{item.answer}</Card.Body>
