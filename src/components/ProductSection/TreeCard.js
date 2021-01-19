ee96a5624a4331479096884fab51abe6aa2a793e
Morgane Kermer <kermer.morgane@gmail.com>
2021-01-19 15:57:22 +0100

    added styling to tree cards

--- src/components/ProductSection/TreeCard.js
+++ src/components/ProductSection/TreeCard.js
@@ -1,8 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./TreeCard.css";
 import CollapseDescription from "./CollapseDiscription";
