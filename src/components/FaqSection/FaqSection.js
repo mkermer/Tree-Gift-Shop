8bcd5b11af21cf97b764f213bbcce463ba2ccff1
Morgane Kermer <kermer.morgane@gmail.com>
2021-01-20 12:47:46 +0100

    Faq expanded

--- src/components/FaqSection/FaqSection.js
+++ src/components/FaqSection/FaqSection.js
@@ -1,48 +1,16 @@
 import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Col, Row } from 'react-bootstrap';
-
-import { ChevronBarDown } from "react-bootstrap-icons";
-
-import faq from './faq';
-
-import './FaqSection.css';
+import { Container, Row } from 'react-bootstrap';
+import FaqQuestion from './FaqQuestion';
+// import './FaqSection.css';
 
 function FaqSection(){
 
     return(
         <>
-        {faq.map((item) => (
-            <Accordion defaultActiveKey="0">
-                <Card className="QuestionCard">
-                    <Card.Header>
-                        <Row> 
-                            <Col md={11}> 
-                                 <Accordion.Toggle as={Button} variant="link" eventKey="1">
-
-                                    {item.question} 
-
-                                </Accordion.Toggle>
-                            </Col>
-                            
-                            <Col md={1}> 
-                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                    <ChevronBarDown size={25} />
-                                </Accordion.Toggle>
-                            </Col>
-                        </Row>
-
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body>{item.answer}</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion> 
-        ))}
+            <Row>
+                <FaqQuestion />
+            </Row>
         </>
-
     )
 }
 
