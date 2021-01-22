5bf267c40a81b4750164111b1ef0341287617d2b
Johanna Horn <j.von.horn@hotmail.com>
2021-01-22 11:06:24 +0100

    making cart into list agaain

--- src/components/CartExtraPage/Cartt.js
+++ src/components/CartExtraPage/Cartt.js
@@ -3,7 +3,7 @@ import { CartContext } from "../../CartContext";
 import { useContext, useEffect } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardImg } from "reactstrap";
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 import "./Cart.css";
@@ -48,54 +48,17 @@ function Cartt(props) {
                 <h2>Your cart</h2>
                 <span>Please complete your purchase</span>
             </div>
-
             <>
                 {cart.map((item) => (
-                    <Card className="cart-items">
-                        <CardBody>
-                            <CardImg src={item.tree_img} alt={item.name} width="10px" />
-                            <CardTitle>{item.tree_name}</CardTitle>
-                            <CardSubtitle>{item.country}</CardSubtitle>
-
-                            <CardText>$ {(item.price * item.count).toFixed(2)} </CardText>
-
-                        
-                                <Button
-                                    className="item-button-remove"
-                                    value={item.id}
-                                    onClick={() => {
-                                        reduceCount(item.tree_id)
-                                    }}>
-                                    -
-                                </Button>
-                       
-                                <span>{item.count}</span>
-                        
-                            <CardText> 
-                                
-                               
-                                <Button
-                                    className="item-button-add"
-                                    value={item.id}
-                                    onClick={() => {
-                                        increaseCount(item.tree_id);
-                                    }}>
-                                    +
-                                </Button>
-                            </CardText>
-                            
-                            <CardText> 
-                                <Button
-                                    value={item.id}
-                                    onClick={() => {
-                                    removeFromCart(item)
-                                    }}>
-                                    remove
-                                </Button> 
-                            </CardText>  
-                        </CardBody>
-                    </Card>
-
+                    <>
+                        <ListGroup class="cd-cart-items">
+                            <ListGroup.Item>
+                                <span class="cd-qty">{item.count}</span> {item.tree_name}
+                                <div class="cd-price">${(item.price * item.count).toFixed(2)}</div>
+                                <a href="#0" class="cd-item-remove cd-img-replace">Remove</a>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </>
                 )
                 )}
             </>
@@ -125,4 +88,4 @@ function Cartt(props) {
 
 }
 
-export default withRouter(Cartt);
+export default withRouter(Cartt)
