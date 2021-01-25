5377d9f09b3670e208c20249f88b0e424337a9f4
Johanna Horn <j.von.horn@hotmail.com>
2021-01-25 12:23:20 +0100

    About us Misssion change

--- src/components/AboutUsSection/MissionJumbotron.js
+++ src/components/AboutUsSection/MissionJumbotron.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 
-import { ReactComponent as MissionImg } from '../../illustrations/undraw_the_world_is_mine_nb0e.svg';
-
-
 export class MissionJumbotron extends Component {
   render() {
     return (
-      <Jumbotron fluid>
-        <Container>
+      <Jumbotron fluid className="MissionGoal">
+        <Container id="MissionGoal-con">
           <Row>
-            <Col lg={{ span: 6, offset: 0 }} md={4} sm={12}>
-              <h6>Our Mission</h6>
+            <Col>
+              <h1>Our Mission</h1>
               <h3>
                 Our philosophy is to create sustainable ecosystems and allow farmers to meet the
                 initial costs of planting trees, ensuring food sovereignty and income opportunities.
               </h3>
             </Col>
-            <Col lg={{ span: 4, offset: 2 }} md={{ span: 7, offset: 1 }} sm={{ span: 10, offset: 1 }} sx={{ span: 4, offset: 6 }}><MissionImg /></Col>
           </Row>
         </Container>
       </Jumbotron>
