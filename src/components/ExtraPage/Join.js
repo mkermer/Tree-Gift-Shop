6b7da62cbb93fee05e9b8554f3d60588c058110e
Morgane Kermer <kermer.morgane@gmail.com>
2021-01-28 21:53:22 +0100

    styling job page

--- src/components/ExtraPage/Join.js
+++ src/components/ExtraPage/Join.js
@@ -1,6 +1,6 @@
 import React,{ useContext, useEffect } from 'react';
 import { ProductContext } from "../../ProductContext";
-import {Accordion, Card, Button, Container} from 'react-bootstrap';
+import {Accordion, Card, Button, Container, Jumbotron} from 'react-bootstrap';
 import './Join.css';
 
 function Join(props){
@@ -10,65 +10,61 @@ useEffect(() => {
     getJobs()
 }, [])
 
-console.log(jobs)
+
 
     const ListJobs = (props) => {
         if(jobs) {
             return jobs.map((obj) => {
 
         return (
-            <div className="Join">
-                <h1>Do you want to join us on our mission?</h1>
-                <h2>We are currently looking for:</h2>
-                    <Container>
-                        <Accordion>
-                            <Card>
-                                <Card.Header>
-                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                <div className="flexb">
-                                        <p>
-                                        {obj.category}
-                                        </p>
-                                        <p>
-                                        {obj.jobtitle}
-                                        </p>
-                                        <p>
-                                        {obj.location}
-                                        </p>
-                                </div>
-                                
-                                </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                <Card.Body>
-                                    <div className="flexb">
-                                        <p className="border">
-                                        <strong>{obj.location}</strong>
-                                        </p>
-                                        <p className="border">
-                                        <strong>{obj.jobtype}</strong>
-                                        </p>
-                                        <p className="border">
-                                        <strong>{obj.salary}$</strong>
-                                        </p>
-                                    </div>
-                                    
-                                    <div className="JobDesc"> 
-                                    {obj.intro}
-                                    <h3>What you'll do in your role</h3>
-                                    {obj.task}
-                                    <h3>The experience you'll bring</h3>
-                                    {obj.qualification}
-                                    </div>
-                                    <Button className="Email"> <a href="mailto:office@co2-treeduce.com?subject=Application" > Apply Now </a> </Button>
-                                    
-                                </Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
-                        </Container>
-
-            </div>
+            <Accordion>
+            <Card>
+            {/* https://svgshare.com/i/TZb.svg */}
+                <Card.Header style={{backgroundImage: 'url(' + obj.image + ')'}}>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                <div className="block">
+                    
+                        <h3 className="title">
+                        {obj.jobtitle}
+                        </h3>
+                        <h3 className="categ">
+                        {obj.category}
+                        </h3>
+                        <h3 className="loc">
+                        {obj.location}
+                        </h3>
+                </div>
+                
+                </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                    
+                    <div className="JobDesc"> 
+                    {obj.intro}
+                    <h3>What you'll do in your role</h3>
+                    {obj.task}
+                    <h3>The experience you'll bring</h3>
+                    {obj.qualification}
+                    </div>
+                    
+                    <div className="flexb">
+                        <p className="border">
+                        <strong>{obj.location}</strong>
+                        </p>
+                        <p className="border">
+                        <strong>{obj.jobtype}</strong>
+                        </p>
+                        <p className="border">
+                        <strong>{obj.salary} $ per month</strong>
+                        </p>
+                    </div>
+                    <Button className="Email"> <a href="mailto:office@co2-treeduce.com?subject=Application" > Apply Now </a> </Button>
+                    
+                </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+            </Accordion>            
         );
       });
         } else {
@@ -77,9 +73,17 @@ console.log(jobs)
       }
 
       return(
-          <div>
-              <ListJobs />
-          </div>
+        <div className="Join">
+        <Jumbotron fluid>
+        <h1>Do you want to join us on our mission?</h1>
+        </Jumbotron>
+        <h2>We are currently looking for:</h2>
+            <Container>
+                
+                    <ListJobs />
+                
+            </Container>
+        </div>
       )
 
 }
