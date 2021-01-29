05b1b115dcedf9e59a3f18650bde13ed41b33060
Johanna Horn <j.von.horn@hotmail.com>
2021-01-29 13:14:17 +0100

    FAQ Question animated

--- src/components/FaqSection/FaqSection.js
+++ src/components/FaqSection/FaqSection.js
@@ -2,15 +2,31 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import FaqQuestion from "./FaqQuestion";
 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+
+Aos.init({});
 
 function FaqSection() {
   return (
     <Container>
     <Col md={12}>
-        <h1>Little FAQ</h1>
-      <p>The answer to your question might be here!</p>
+        <h1 
+          data-aos="fade-right"
+          data-aos-offset="200"
+          data-aos-duration="500"
+        >Little FAQ</h1>
+      <p
+          data-aos="fade-right"
+          data-aos-offset="250"
+          data-aos-duration="500"
+      >The answer to your question might be here!</p>
     </Col>
-      <Row>
+      <Row 
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-duration="550">
         <FaqQuestion />
       </Row>
     </Container>
