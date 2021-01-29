d710eb53744b168955c6fe59bc13104a48a0bf4c
Johanna Horn <j.von.horn@hotmail.com>
2021-01-29 13:19:06 +0100

    Contact Section animated

--- src/components/ContactFormSection/ContactSection.js
+++ src/components/ContactFormSection/ContactSection.js
@@ -3,17 +3,29 @@ import { Container, Row, Col} from 'react-bootstrap';
 import ContactForm from './ContactForm';
 import { ReactComponent as Contact } from '../../illustrations/undraw_contact_us_15o2.svg';
 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+
+Aos.init({});
 
 function ContactSection() {
 
         return (
             <Container>
                 <Row>
-
-                    <Col lg={{ span: 8, offset: 2 }} md={{ span: 10, offset: 1 }} sm={12} xs={12}>
+                    <Col 
+                        data-aos="zoom-in-up"
+                        data-aos-offset="500"
+                        data-aos-duration="600"
+                        lg={{ span: 8, offset: 2 }} md={{ span: 10, offset: 1 }} sm={12} xs={12}>
                         <Contact />
                     </Col>
-                    <Col lg={{ span: 8, offset: 2 }} md={{ span: 10, offset: 1 }} sm={12} xs={12}>
+                    <Col
+                        data-aos="zoom-in-up"
+                        data-aos-offset="400"
+                        data-aos-duration="600"
+                        lg={{ span: 8, offset: 2 }} md={{ span: 10, offset: 1 }} sm={12} xs={12}>
 
                     <h4>Contact Us</h4>
                      <ContactForm />
